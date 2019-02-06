047663beb480329a94a5a04df56bf8ca43e66a36
Laura <laurao148@gmail.com>
2019-02-06 04:44:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
+<<<<<<< HEAD
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validate :title_has_certain_words
 
@@ -15,6 +16,20 @@ class Post < ActiveRecord::Base
     if self.title.to_s.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
       return true
     else
+=======
+  validates :category, inclusion: {in: %w(fiction non-fiction)}
+  validate :title_has_certain_words
+
+  VALID_WORDS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+    ]
+
+  def title_has_certain_words
+    if VALID_WORDS.none?{ |t| t.match title}
+>>>>>>> ade4dc92ebc64c73128d51c307f1995f658ef432
       errors.add(:title, "Invalid")
     end
   end
